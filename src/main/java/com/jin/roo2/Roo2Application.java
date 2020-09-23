bad6a215d62e86f58f4f7bb6900ce1cfec8ca105
Jinu Vijay <jinu.vijay@gmail.com>
2020-09-23 00:30:41 -0400

    Initial file set

---
+++ src/main/java/com/jin/roo2/Roo2Application.java
@@ -0,0 +1,23 @@
+package com.jin.roo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * = Roo2Application
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@SpringBootApplication
+public class Roo2Application {
+
+    /**
+     * TODO Auto-generated method documentation
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Roo2Application.class, args);
+    }
+}
